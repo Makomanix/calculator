c75b814c117ca505b6a6ce87daa4722731e5c91e
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-01-19 16:17:12 -0500

    key press /  mouse click evalution working

--- calc.js
+++ calc.js
@@ -1,31 +1,50 @@
 document.addEventListener("keydown", toggleOnOff);
+
 const power = document.querySelector('.power');
 power.addEventListener('click', toggleOnOff);
+
+const buttons = document.querySelector('.buttons-grid');
 
 let onOff = false;
 
 function toggleOnOff(e) {
-    console.log('hi')
-    if (e.key === "p" || e.target.innerText === 'Pow') {
-        onOff ? onOff = false : onOff = true;
-        if (onOff == true) {
+  if (e.key === "p" || e.target.innerText === 'Pow') {
+    onOff ? onOff = false : onOff = true;
+    console.log(onOff)
+    if (onOff == true) {
             activate();
         } else {
-            console.log('hi')
             deactivate();
         }
-    }
-    
+    }    
 };
 
 function activate() {
-    document
-    .querySelectorAll('.buttons-grid')
-    .addEventListener('click', function(e) {
-        
-    })
+  console.log('activated');
+  buttons.addEventListener('click', handleClick);
+  document.addEventListener('keydown', handlePress);
 };
 
-// function handleClick(e) {
-//     console.log(e.target.value);
-// }
+function deactivate() {
+  console.log('deactivated');
+  buttons.removeEventListener('click', handleClick);
+  document.removeEventListener('keydown', handlePress);
+};
+
+function handleClick(e){
+  handleButton(e.target.innerText);
+}
+
+function handlePress(e) {
+  handleButton(e.key);
+}
+
+function handleButton(value) {
+  if ( isNaN(parseInt(value)) ) {
+    console.log('symbol');
+    console.log(value);
+  } else {
+    console.log('number')
+    console.log(value);
+  }
+}
