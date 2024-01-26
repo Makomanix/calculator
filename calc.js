612a3afa350a3df00105409df926bf3e4dc8018d
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-01-26 16:32:39 -0500

    refactored rerender(), activation(), screen functional

--- calc.js
+++ calc.js
@@ -1,10 +1,96 @@
-const equation = document.querySelector('.equation');
-// max length 30
-
-const solution = document.querySelector('.solution');
-// max length 15 then use powers
-
+let buffer = '';
+let memory = '';
+let lastOperator = '';
+let equal = false;
 let onOff = false;
+
+const buttonValues = document.querySelectorAll("button");
+
+let buttonValueArray = Array.from(buttonValues).map((button) => {
+  return button.value;
+});
+
+
+function handleClick(e){
+  handleButton(e.target.value);
+  e.target.blur();
+};
+
+function handlePress(e) {
+  handleButton(e.key);
+};
+
+function handleButton(value) {
+  if ( isNaN(parseInt(value)) && value != '.' ) {
+    if (checkSymbol(value)) {
+      handleSymbol(value);
+    } else {
+      return;
+    }
+  } else {
+    handleNumber(value);
+  }
+};
+
+
+function checkSymbol(symbol) {
+  console.log(symbol);
+  (!(buttonValueArray.includes(symbol))) ? false : true
+};
+
+function handleSymbol(symbol){
+  
+  switch (symbol) {
+    case "=":
+      break;
+      
+    case "Backspace":
+      break;
+        
+    case "Delete":
+      break;
+          
+    case "+":
+    case "-":
+    case "/":
+    case "*":
+    case "^":
+    case "!":
+    case "√":
+      break;
+  }
+};
+
+
+function handleNumber(number) {
+  console.log(buffer)
+  if (buffer === '0') {
+    buffer = number;
+  } else {
+    buffer += number
+  }
+  rerender();
+};
+
+
+function rerender() {
+  const solution = document.querySelector('.solution');
+  // max length 15
+  const equation = documnet.querySelector('.equation');
+  // max length 30
+
+  if (onOff == false) {
+    buffer = '';
+    memory = '';
+  }  
+  
+  if (buffer === '' && onOff == true) {
+      buffer = '0';
+  } 
+    
+    solution.innerText = buffer;
+  
+};
 
 function toggleOnOff(e) {
   if (e.key === "p" || e.target.innerText === 'Pow') {
@@ -18,94 +104,27 @@ function toggleOnOff(e) {
 };
 
 function activation(bool) {
-
+  
   const buttons = document.querySelector(".buttons-grid");
-
+  
   if (bool == true) {
-
+    
     buttons.addEventListener('click', handleClick);
     document.addEventListener('keydown', handlePress);
-    solution.innerText = '0'
-
+    rerender();
+    
   } else if (bool == false) {
-
+    
     buttons.removeEventListener("click", handleClick);
     document.removeEventListener("keydown", handlePress);
-    solution.innerText = "";
-    equation.innerText = "";
-  }
-};
-
-function handleClick(e){
-  handleButton(e.target.value);
-  e.target.blur();
-};
-
-function handlePress(e) {
-  handleButton(e.key);
-};
-
-function handleButton(value) {
-  if ( isNaN(parseInt(value)) && value != '.') {
-    if (getButtonValueArray(value)) {
-      handleSymbol(value);
-    } else {
-      return;
-    }
-  } else {
-    handleNumber(value);
-  }
-};
-
-function handleSymbol(symbol){
-  
-  switch (symbol) {
-    case "=":
-      break;
-
-    case "Backspace":
-      break;
-
-    case "Delete":
-      break;
-
-    case "+":
-    case "-":
-    case "/":
-    case "*":
-    case "^":
-    case "!":
-    case "√":
-      
-  }
-};
-
-
-function handleNumber(number) {
-  solution.innerText = number;
-};
-
-
-function getButtonValueArray(value) {
-
-  const buttonValues = document.querySelectorAll('button');
-
-  let buttonValueArray = Array.from(buttonValues).map((button) => {
-      return button.value;
-  });
-  
-  buttonValueArray.push("Enter");
-
-  if ( !(buttonValueArray.includes(value)) ) {
-    return false;
-  } else {
-    return true;
+    rerender();
+    
   }
 };
 
 function init() {
   document.addEventListener("keydown", toggleOnOff);
-
+  
   const power = document.querySelector(".power");
   power.addEventListener("click", toggleOnOff);
 };
